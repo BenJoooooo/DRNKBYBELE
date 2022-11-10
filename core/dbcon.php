f7bf6c53846309b928e1968034abc1fe216ad52f
BenJoooooo <benjo.solanor@gmail.com>
2022-11-11 06:52:54 +0800

    Added remote database and configured mysql workbench to host a remote database, development connection is commented

--- core/dbcon.php
+++ core/dbcon.php
@@ -1,9 +1,17 @@
 <?php
 
-    $host = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "drnkdup";
+    // Development Connection
+
+    // $host = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "drnkdup";
+
+    // Remote database connection
+    $host = "remotemysql.com";
+    $username = "S6YgyM4GtY";
+    $password = "0vMcqT6aA1";
+    $database = "S6YgyM4GtY";
 
     // Creating database connection
     $con = mysqli_connect($host, $username, $password, $database);
