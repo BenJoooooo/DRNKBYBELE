34d07644dd045a00411d87e3fdf621e7a7dac2be
Ben Joshua Solanor <benjo.solanor@gmail.com>
2022-11-30 01:35:59 +0800

    Added responsive to login and signup, fixed border and margin issues to containers

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
     include ("functions/userFunctions.php");
 
     if(isset($_SESSION['auth'])) {
@@ -9,42 +8,40 @@
     }
 
     require ("includes/header.php");
-
 ?>
-
-    <div class="signup-container">
-       
-        <!-- Session Message -->
-        <?php include('functions/sessionmessage.php'); ?>
+    <div class="signup-wrapper">
+        <div class="signup-container">
         
-        <div class="signup-header">
-            <h1 class="sign-up-create-account">Login</h1>
-            <p class="sign-up-already-have">Don't have an account? <a href="signup.php">Register</a></p>
-        </div>
+            <!-- Session Message -->
+            <?php include('functions/sessionmessage.php'); ?>
+            
+            <div class="signup-header">
+                <h1 class="sign-up-create-account">Login</h1>
+                <p class="sign-up-already-have">Don't have an account? <a href="signup.php">Register</a></p>
+            </div>
 
-        <div class="signup-form-container">
-            <form action="functions/authcode.php" method="POST" class="signup-form">
-                <div class="signup email">
-                    <label for="">Email</label>
-                    <input type="email" name="login_email" class="signup-input email" required placeholder="Enter your email">
-                </div>
-                <div class="signup password">
-                    <label for="">Password</label>
-                    <div class="password-container">
-                        <input type="password" name="login_password" id="password" class="signup-input password" required placeholder="Enter your password">
-                        <i class="fa fa-eye" id="show-password"></i>
+            <div class="signup-form-container">
+                <form action="functions/authcode.php" method="POST" class="signup-form">
+                    <div class="signup email">
+                        <label for="">Email</label>
+                        <input type="email" name="login_email" class="signup-input email" required placeholder="Enter your email">
                     </div>
-                </div>
-                
-                <button class="signup-submit" name="login_submit">Login</button>
-            </form>
+                    <div class="signup password">
+                        <label for="">Password</label>
+                        <div class="password-container">
+                            <input type="password" name="login_password" id="password" class="signup-input password" required placeholder="Enter your password">
+                            <i class="fa fa-eye" id="show-password"></i>
+                        </div>
+                    </div>
+                    
+                    <button class="signup-submit" name="login_submit">Login</button>
+                </form>
+            </div>
         </div>
     </div>
     
     <script src="functions/passwordIcon.js"></script>
 
 <?php
-
     require ("includes/footer.php");
-
 ?>
--- signup.php
+++ signup.php
@@ -11,44 +11,45 @@
 
     include "includes/header.php";
 ?>
-    
-    <div class="signup-container">
+    <div class="signup-wrapper">
+        <div class="signup-container">
 
-        <!-- Session Message -->
-        <?php include('functions/sessionmessage.php'); ?>
+            <!-- Session Message -->
+            <?php include('functions/sessionmessage.php'); ?>
 
-        <div class="signup-header">
-            <h1 class="sign-up-create-account">Create an account</h1>
-            <p class="sign-up-already-have">Already have an account? <a href="login.php">Log in</a></p>
-        </div>
+            <div class="signup-header">
+                <h1 class="sign-up-create-account">Create an account</h1>
+                <p class="sign-up-already-have">Already have an account? <a href="login.php">Log in</a></p>
+            </div>
 
-        <div class="signup-form-container">
-            <form action="functions/authcode.php" method="POST" class="signup-form">
-                <div class="signup fullname">
-                    <label for="">Full Name</label>
-                    <input type="text" name="signup_fullname" class="signup-input name" required placeholder="Enter Full Name">
-                </div>
-                <div class="signup email">
-                    <label for="">Email</label>
-                    <input type="email" name="signup_email" class="signup-input email" required placeholder="Enter Email">
-                </div>
-                <div class="signup password">
-                    <label for="">Password</label>
-                    <div class="password-container">
-                        <input type="password" name="signup_password" id="password" class="signup-input password" required placeholder="Password">
-                        <i class="fa fa-eye" id="show-password"></i>
+            <div class="signup-form-container">
+                <form action="functions/authcode.php" method="POST" class="signup-form">
+                    <div class="signup fullname">
+                        <label for="">Full Name</label>
+                        <input type="text" name="signup_fullname" class="signup-input name" required placeholder="Enter Full Name">
                     </div>
-                </div>
-                <div class="signup repeat-password">
-                    <label for="">Repeat Password</label>
-                    <div class="password-container">
-                        <input type="password" name="repeat_signup_password" id="repeatPassword" class="signup-input repeat-password" required placeholder="Confirm Password">
-                        <i class="fa fa-eye" id="show-repeat-password"></i>
+                    <div class="signup email">
+                        <label for="">Email</label>
+                        <input type="email" name="signup_email" class="signup-input email" required placeholder="Enter Email">
                     </div>
-                </div>
-                
-                <button class="signup-submit" name="signup_submit">Submit</button>
-            </form>
+                    <div class="signup password">
+                        <label for="">Password</label>
+                        <div class="password-container">
+                            <input type="password" name="signup_password" id="password" class="signup-input password" required placeholder="Password">
+                            <i class="fa fa-eye" id="show-password"></i>
+                        </div>
+                    </div>
+                    <div class="signup repeat-password">
+                        <label for="">Repeat Password</label>
+                        <div class="password-container">
+                            <input type="password" name="repeat_signup_password" id="repeatPassword" class="signup-input repeat-password" required placeholder="Confirm Password">
+                            <i class="fa fa-eye" id="show-repeat-password"></i>
+                        </div>
+                    </div>
+                    
+                    <button class="signup-submit" name="signup_submit">Submit</button>
+                </form>
+            </div>
         </div>
     </div>
 
